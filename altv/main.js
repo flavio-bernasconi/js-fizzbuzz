4f9132c95f40c0c80333f9e8578bf94c98c79ec3
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-04 14:22:07 +0200

    second commit

--- altv/main.js
+++ altv/main.js
@@ -2,6 +2,7 @@
 
 var tre = 'fuzz';
 var cinque = 'buzz';
+var contenuto;
 
 
 
@@ -14,23 +15,37 @@ for (var i = 1; i <= 100; i++) {
   //stampo fuzz e buzz e skippo di uno l-indice
   if (buzz == 0 && fuzz == 0) {
     console.log(i,tre,cinque);
+
+    contenuto = document.getElementById('numero').innerHTML;
+    document.getElementById('numero').innerHTML  = contenuto + "<li>" + i + " " + tre + cinque + "</li>";
+
     continue;
   }
   // se il resto fuzz 3 è uguale a 0  stampo fuzz
   if (fuzz == 0) {
     console.log( i,tre);
+
+    contenuto = document.getElementById('numero').innerHTML;
+    document.getElementById('numero').innerHTML  = contenuto + "<li>" + i + " " + tre + "</li>";
+
   }
   // se il resto buzz 5 è uguale a 0  stampo buzz
   if (buzz == 0) {
     console.log( i,cinque);
+
+    contenuto = document.getElementById('numero').innerHTML;
+    document.getElementById('numero').innerHTML  = contenuto + "<li>" + i + " " + cinque + "</li>";
+
   }
 
   // se il resto è diverso da 0 sia per 5 che 3 allora stampo l-indice
   if (buzz != 0 && fuzz != 0) {
     console.log(i);
+    contenuto = document.getElementById('numero').innerHTML;
+    document.getElementById('numero').innerHTML  = contenuto + "<li>" + i + " " + "</li>";
+
   }
 }
-
 
 
 
