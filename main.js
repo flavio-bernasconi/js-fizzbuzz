943e1e0be5102e825a816c078d228be58ee88a6b
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-04 14:13:53 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+var testo;
+var tre = 'fuzz';
+var cinque = 'buzz';
+
+
+
+for (var i = 1; i <= 100; i++) {
+  // resto dell-indice diviso 3 e 5
+  var fuzz = i % 3;
+  var buzz = i % 5;
+
+  // se il resto di buzz e fuzz è 0 per entrambi
+  //stampo fuzz e buzz e skippo di uno l-indice
+  if (buzz == 0 && fuzz == 0) {
+    console.log(i,tre,cinque);
+    continue;
+  }
+  // se il resto fuzz 3 è uguale a 0  stampo fuzz
+  if (fuzz == 0) {
+    console.log( i,tre);
+  }
+  // se il resto buzz 5 è uguale a 0  stampo buzz
+  if (buzz == 0) {
+    console.log( i,cinque);
+  }
+
+  // se il resto è diverso da 0 sia per 5 che 3 allora stampo l-indice
+  if (buzz != 0 && fuzz != 0) {
+    console.log(i);
+  }
+}
+
+
+
+
+
+
+
+//
